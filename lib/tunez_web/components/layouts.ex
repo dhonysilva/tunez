adcc3905fac4f2e6ac72db18c4f898fe51d1be25
Dhony Silva <dhony.web@gmail.com>
2025-01-17 15:22:18 -0300

    Init tunez project and install ingiter and ash

---
+++ lib/tunez_web/components/layouts.ex
@@ -0,0 +1,5 @@
+defmodule TunezWeb.Layouts do
+  use TunezWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ lib/tunez_web/controllers/page_html.ex
@@ -0,0 +1,5 @@
+defmodule TunezWeb.PageHTML do
+  use TunezWeb, :html
+
+  embed_templates "page_html/*"
+end
