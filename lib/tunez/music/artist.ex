c1e8b4edd5f4a7c325c2f2264554df20adb88b3e
Dhony Silva <dhony.web@gmail.com>
2025-01-17 16:07:32 -0300

    Add sqlite instruction to resourse Tunez.Music.Artist

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -1,3 +1,8 @@
 defmodule Tunez.Music.Artist do
   use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshSqlite.DataLayer
+
+  sqlite do
+    table "artists"
+    repo Tunez.Repo
+  end
 end
