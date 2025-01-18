e3aeb6a1d78c4370684e1b84b359a731769b35a9
Dhony Silva <dhony.web@gmail.com>
2025-01-17 22:16:33 -0300

    Define attributes for resource Album and relationships

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -1,3 +1,26 @@
 defmodule Tunez.Music.Album do
   use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshSqlite.DataLayer
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string do
+      allow_nil? false
+    end
+
+    attribute :year_released, :integer do
+      allow_nil? false
+    end
+
+    attribute :cover_image_url, :string
+
+    create_timestamp :inserted_at
+    create_timestamp :updated_at
+  end
+
+  relationships do
+    belongs_to :artist, Tunez.Music.Artist do
+      allow_nil? false
+    end
+  end
 end
