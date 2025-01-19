b4cf9c071b4e3efc075ed9fbeea152b479ae365d
Dhony Silva <dhony.web@gmail.com>
2025-01-19 12:49:59 -0300

    Starts with Calculations

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -54,5 +54,13 @@ defmodule Tunez.Music.Album do
     end
   end
 
+  calculations do
+    calculate :years_ago, :integer, expr(2025 - year_released)
+
+    calculate :string_years_ago,
+              :string,
+              expr("wow, this was released " <> years_ago <> " years ago!")
+  end
+
   def next_year, do: Date.utc_today().year + 1
 end
